1b78e3ff7a296394de973fcc09ad83e81c532fad
jkbngb <86148395+jkbngb@users.noreply.github.com>
2023-01-26 19:11:34 +0100

    Update FormCalcApplicationTests.java

--- src/test/java/at/fhtechnikum/FormCalc/FormCalcApplicationTests.java
+++ src/test/java/at/fhtechnikum/FormCalc/FormCalcApplicationTests.java
@@ -1,13 +1,22 @@
 package at.fhtechnikum.FormCalc;
 
+import at.fhtechnikum.FormCalc.Controller.FormController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FormCalcApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		FormController controllerCirc = new FormController();
+		float resultCircle = controllerCirc.calcCircle(5);
+		assertEquals(resultCircle, 78.5);
 
+		FormController controllerSquare = new FormController();
+		int resultSquare = controllerSquare.calcSquare(4);
+		assertEquals(resultSquare, 16);
+	}
 }
