c7539e53845144fc57fb4de888569b2d5c512789
ic20b026 <ic20b026@technikum-wien.at>
2023-01-26 17:55:10 +0100

    Initial commit

---
+++ src/main/java/at/fhtechnikum/FormCalc/FormCalcApplication.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.FormCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtechnikum/FormCalc/FormCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.FormCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
