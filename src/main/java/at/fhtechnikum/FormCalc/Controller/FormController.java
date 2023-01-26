4e77c103ccbf0069496b21e412b23be5b50b490f
jkbngb <86148395+jkbngb@users.noreply.github.com>
2023-01-26 19:12:31 +0100

    Update FormController.java

--- src/main/java/at/fhtechnikum/FormCalc/Controller/FormController.java
+++ src/main/java/at/fhtechnikum/FormCalc/Controller/FormController.java
@@ -1,4 +1,18 @@
 package at.fhtechnikum.FormCalc.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FormController {
+    @GetMapping("/api/circle/{radius}")
+    public float calcCircle(@PathVariable int radius){
+        return (float) (3.14 * radius * radius);
+    }
+
+    @GetMapping("/api/square/{length}")
+    public int calcSquare(@PathVariable int length){
+        return length * length;
+    }
 }
